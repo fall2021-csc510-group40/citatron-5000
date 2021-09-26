2066f5b30d2d4e65681f6e9d2b7588fe97534641
agcarrut <agcarrut@ncsu.edu>
2021-09-26 18:24:59 -0400

    Created plain text formatter and updated to handle missing fields

--- core/formatter/formatter.go
+++ core/formatter/formatter.go
@@ -4,35 +4,80 @@ import (
 	"core/schema"
 	"strconv"
 	"strings"
+	"time"
 )
 
 func BibtexFormat(paper *schema.Work) string {
 	var citation string = "@article{"
-	for _, author := range paper.Authors {
-		citation += author + "_"
-	}
-	citation += strconv.Itoa(paper.Year) + ", title={"
-	citation += paper.Title + "}, DOI={"
-	citation += paper.DOI + "}, journal={"
-	citation += paper.Venue + "}, author={"
-	for i, author := range paper.Authors {
-		parts := strings.Split(author, " ")
-		if len(parts) > 0 {
-			citation += parts[1] + ", " + parts[0]
-		} else {
-			citation += parts[0]
-		}
-		if i != (len(paper.Authors) - 1) {
-			citation += "and"
+	if len(paper.Authors) > 0 {
+		for _, author := range paper.Authors {
+			citation += author + "_"
 		}
 	}
-	citation += "}, year={" + strconv.Itoa(paper.Year) + "}}"
+	if paper.Year != 0 {
+		citation += strconv.Itoa(paper.Year)
+	}
+	if paper.Year == 0 && len(paper.Authors) == 0 {
+		citation += "empty"
+	}
+	if len(paper.Title) > 0 {
+		citation += ", title={" + paper.Title + "}, "
+	}
+	if len(paper.DOI) > 0 {
+		citation += "DOI={" + paper.DOI + "}, "
+	}
+	if len(paper.Venue) > 0 {
+		citation += "journal={" + paper.Venue + "}, "
+	}
+	if len(paper.Authors) > 0 {
+		citation += "author={"
+		for i, author := range paper.Authors {
+			parts := strings.Split(author, " ")
+			if len(parts) > 1 {
+				citation += parts[1] + ", " + parts[0]
+			} else {
+				citation += parts[0]
+			}
+			if i != (len(paper.Authors) - 1) {
+				citation += "and"
+			}
+		}
+		citation += "}, "
+	}
 
+	if paper.Year != 0 {
+		citation += "year={" + strconv.Itoa(paper.Year) + "}}"
+	}
 	return citation
 }
 
 func PlaintextFormat(paper *schema.Work) string {
 	var citation string = ""
-
+	if len(paper.Authors) > 0 {
+		for i, author := range paper.Authors {
+			parts := strings.Split(author, " ")
+			if len(parts) > 1 {
+				citation += string(parts[0][0]) + ". " + parts[1]
+			} else {
+				citation += parts[0]
+			}
+			if i == (len(paper.Authors) - 2) {
+				citation += "and"
+			}
+		}
+		citation += ", "
+	}
+	if len(paper.Title) > 0 {
+		citation += "\"" + paper.Title + ",\""
+	}
+	if len(paper.Venue) > 0 {
+		citation += paper.Venue + ", "
+	}
+	if paper.Month != 0 {
+		citation += time.Month(paper.Month).String() + ", "
+	}
+	if paper.Year != 0 {
+		citation += strconv.Itoa(paper.Year)
+	}
 	return citation
 }
