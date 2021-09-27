9dbfada5fa16e11c9fbacebfde20fae1dfa5dcda
kunalpatange <kunalpatange1111@gmail.com>
2021-09-27 15:31:43 -0400

    modified bibtex formatter

--- core/formatter/formatter.go
+++ core/formatter/formatter.go
@@ -8,46 +8,63 @@ import (
 )
 
 func BibtexFormat(paper *schema.Work) string {
-	var citation string = "@article{"
+	var citation string = "@article{ \n" //need to add the key
+
 	if len(paper.Authors) > 0 {
-		for _, author := range paper.Authors {
-			citation += author + "_"
+		citation += "Author= { "
+		for i := 0; i < len(paper.Authors)-1; i++ {
+			citation += paper.Authors[i] + ", "
 		}
+		citation += paper.Authors[len(paper.Authors)-1]
+		citation += " },\n"
 	}
-	if paper.Year != 0 {
-		citation += strconv.Itoa(paper.Year)
-	}
-	if paper.Year == 0 && len(paper.Authors) == 0 {
-		citation += "empty"
-	}
+
 	if len(paper.Title) > 0 {
-		citation += ", title={" + paper.Title + "}, "
+		citation += "Title= { " + paper.Title + " },\n"
 	}
+
 	if len(paper.DOI) > 0 {
-		citation += "DOI={" + paper.DOI + "}, "
+		citation += "DOI= { " + paper.DOI + " },\n"
 	}
+
+	if len(paper.Arxiv) > 0 {
+		citation += "ARXIV= { " + paper.Arxiv + " },\n"
+	}
+	if len(paper.ISBN) > 0 {
+		citation += "ISBN= { " + paper.Arxiv + " },\n"
+	}
+
 	if len(paper.Venue) > 0 {
-		citation += "journal={" + paper.Venue + "}, "
+		citation += "Journal= { " + paper.Venue + " },\n"
 	}
-	if len(paper.Authors) > 0 {
-		citation += "author={"
-		for i, author := range paper.Authors {
-			parts := strings.Split(author, " ")
-			if len(parts) > 1 {
-				citation += parts[1] + ", " + parts[0]
-			} else {
-				citation += parts[0]
-			}
-			if i != (len(paper.Authors) - 1) {
-				citation += "and"
-			}
-		}
-		citation += "}, "
+	if paper.Month != 0 {
+		citation += "Month= { " + strconv.Itoa(paper.Year) + " }, \n"
 	}
 
 	if paper.Year != 0 {
-		citation += "year={" + strconv.Itoa(paper.Year) + "}}"
+		citation += "Year= { " + strconv.Itoa(paper.Year) + " }, \n"
 	}
+
+	if paper.Page != "" {
+		citation += "Page= { " + paper.Page + " }, \n"
+	}
+
+	// need to check this logic
+	// if len(paper.Authors) > 0 {
+	// 	citation += "author={"
+	// 	for i, author := range paper.Authors {
+	// 		parts := strings.Split(author, " ")
+	// 		if len(parts) > 1 {
+	// 			citation += parts[1] + ", " + parts[0]
+	// 		} else {
+	// 			citation += parts[0]
+	// 		}
+	// 		if i != (len(paper.Authors) - 1) {
+	// 			citation += "and"
+	// 		}
+	// 	}
+	citation += "},\n"
+	// }
 	return citation
 }
 
